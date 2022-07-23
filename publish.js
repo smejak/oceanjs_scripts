85ff56920045ed74e08ae20b588280e7e5a8c19f
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-23 13:54:53 +0200

    WIP: added constants

--- publish.js
+++ publish.js
@@ -28,3 +28,28 @@ const {
 } = require('@oceanprotocol/lib')
 
 console.log(ZERO_ADDRESS)
+
+let config
+let aquarius
+let providerUrl
+let publisherAccount
+let consumerAccount
+let stakerAccount
+let addresses
+let poolNftAddress
+let poolDatatokenAddress
+let poolAddress
+let freNftAddress
+let freDatatokenAddress
+let freAddress
+let freId
+let dispenserNftAddress
+let dispenserDatatokenAddress
+let dispenserAddress
+
+const POOL_NFT_NAME = 'Datatoken 1'
+const POOL_NFT_SYMBOL = 'DT1'
+const FRE_NFT_NAME = 'Datatoken 2'
+const FRE_NFT_SYMBOL = 'DT2'
+const DISP_NFT_NAME = 'Datatoken 3'
+const DISP_NFT_SYMBOL = 'DT3'
