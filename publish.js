b767d8a03e12d8eac044599e2a946f6031564311
Jakub Smékal <jakub.smekal@gmail.com>
2022-07-23 13:55:49 +0200

    WIP: added algo_url and ddo

--- publish.js
+++ publish.js
@@ -53,3 +53,42 @@ const FRE_NFT_NAME = 'Datatoken 2'
 const FRE_NFT_SYMBOL = 'DT2'
 const DISP_NFT_NAME = 'Datatoken 3'
 const DISP_NFT_SYMBOL = 'DT3'
+
+const ASSET_URL = {
+    datatokenAddress: '0x0',
+    nftAddress: '0x0',
+    files: [
+      {
+        type: 'url',
+        url: 'https://raw.githubusercontent.com/oceanprotocol/testdatasets/main/shs_dataset_test.txt',
+        method: 'GET'
+      }
+    ]
+}
+
+const DDO = {
+    '@context': ['https://w3id.org/did/v1'],
+    id: '',
+    version: '4.1.0',
+    chainId: 4,
+    nftAddress: '0x0',
+    metadata: {
+      created: '2021-12-20T14:35:20Z',
+      updated: '2021-12-20T14:35:20Z',
+      type: 'dataset',
+      name: 'dataset-name',
+      description: 'Ocean protocol test dataset description',
+      author: 'oceanprotocol-team',
+      license: 'MIT'
+    },
+    services: [
+      {
+        id: 'testFakeId',
+        type: 'access',
+        files: '',
+        datatokenAddress: '0x0',
+        serviceEndpoint: 'https://v4.provider.rinkeby.oceanprotocol.com',
+        timeout: 0
+      }
+    ]
+ }
